1453d88eca5561f13dcf7ad749f0652e9a117f4c
Harlan <wawacry@qq.com>
2022-11-19 12:54:53 +0800

    commit

--- src/jsonrpc2.rs
+++ src/jsonrpc2.rs
@@ -8,11 +8,17 @@ use serde::ser::{SerializeStruct, Serializer};
 use serde::Deserialize;
 use serde::Serialize;
 use std::result::Result as StdResult;
+use std::sync::atomic::AtomicU64;
+use tokio::sync::mpsc::UnboundedReceiver;
 
+use beef::Cow;
+use serde::de::DeserializeOwned;
+use serde_json::value::RawValue;
 use serde_json::Deserializer;
 
 use std::collections::HashMap;
 use std::str;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::sync::{broadcast, mpsc, oneshot};
@@ -29,40 +35,140 @@ pub trait JsonRpc2 {
     fn close() -> Result<()>;
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
 struct Request {
     #[serde(rename = "jsonrpc2")]
-    header: String,
+    jsonrpc2: String,
     #[serde(rename = "method")]
     method: String,
     #[serde(rename = "params", skip_serializing_if = "Option::is_none")]
-    params: Option<serde_json::Value>,
+    params: Option<String>,
     //if id is none , then it is a notification
     //https://www.jsonrpc.org/specification#notification
     #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
-    id: Option<ID>,
+    id: Option<Id>,
+}
+
+impl Request {
+    pub fn new(method: String, params: Option<String>, id: Option<Id>) -> Self {
+        Self {
+            jsonrpc2: "2.0".to_string(),
+            method,
+            params,
+            id,
+        }
+    }
+
+    pub fn marshal_json(&self) -> Result<String> {
+        let data = serde_json::to_string(self)?;
+        Ok(data)
+    }
+    pub fn unmarshal_json(&mut self, data: &String) -> Result<()> {
+        let request: Request = serde_json::from_str(data)?;
+        self.jsonrpc2 = request.jsonrpc2;
+        self.method = request.method;
+        self.params = request.params;
+        self.id = request.id;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::Id;
+
+    use std::str::FromStr;
+
+    use super::Request;
+    use super::Response;
+
+    use super::AnyMessage;
+    //https://juejin.cn/post/6844904105698148360
+    #[test]
+    fn test_request() {
+        let method = String::from_str("add").unwrap();
+        let params = String::from_str("[1,2]").unwrap();
+        let id = Id::Number(3);
+
+        let request = Request::new(method, Some(params), Some(id));
+        let serialized = request.marshal_json().unwrap();
+
+        assert_eq!(
+            serialized,
+            r#"{"jsonrpc2":"2.0","method":"add","params":"[1,2]","id":3}"#
+        );
+
+        let mut default = Request::default();
+        default.unmarshal_json(&serialized).unwrap();
+
+        assert_eq!(request, default);
+    }
+    #[test]
+    fn test_any_message() {
+        let method = String::from_str("add").unwrap();
+        let params = String::from_str("[1,2]").unwrap();
+        let id = Id::Number(3);
+        let request = Request::new(method, Some(params), Some(id));
+
+        println!("marshal request: {}", request.marshal_json().unwrap());
+
+        let any1 = AnyMessage::Request(request);
+
+        let marshal_msg = serde_json::to_string(&any1).unwrap();
+        println!("marshal AnyMessage request: {}", marshal_msg);
+
+        let data: AnyMessage = serde_json::from_str(&marshal_msg).unwrap();
+
+        match data {
+            AnyMessage::Request(req) => {
+                println!("marshal request 2: {}", req.marshal_json().unwrap());
+            }
+            AnyMessage::Response(res) => {
+                println!("marshal response 2: {}", res.marshal_json().unwrap());
+            }
+        }
+
+        let id2 = Id::Str(String::from_str("3").unwrap());
+        let result = Some(String::from_str("[2,3]").unwrap());
+        let response = Response::new(id2, result, None);
+
+        println!("marshal response: {}", response.marshal_json().unwrap());
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Response {
     #[serde(rename = "jsonrpc2")]
-    header: String,
+    jsonrpc2: String,
     #[serde(rename = "id")]
-    id: ID,
+    id: Id,
     #[serde(rename = "result", skip_serializing_if = "Option::is_none")]
-    result: Option<serde_json::Value>,
+    result: Option<String>,
     #[serde(rename = "error", skip_serializing_if = "Option::is_none")]
     error: Option<Error>,
 }
 
 impl Response {
-    fn marshal_json(&self) -> Result<Vec<u8>> {
-        if (self.result.is_none() || self.result.clone().unwrap().is_null()) && self.error.is_none()
-        {
-            return Err(JsonError::ErrCanNotMarshalResponse);
+    pub fn new(id: Id, result: Option<String>, error: Option<Error>) -> Self {
+        Self {
+            jsonrpc2: "2.0".to_string(),
+            result,
+            error,
+            id,
         }
-        let val = serde_json::to_vec(self)?;
-        Ok(val)
+    }
+    pub fn marshal_json(&self) -> Result<String> {
+        let data = serde_json::to_string(self)?;
+        Ok(data)
+    }
+    pub fn unmarshal_json(&mut self, data: &String) -> Result<()> {
+        let response: Response = serde_json::from_str(data)?;
+        self.jsonrpc2 = response.jsonrpc2;
+        self.result = response.result;
+        self.error = response.error;
+        self.id = response.id;
+        Ok(())
     }
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -74,52 +180,32 @@ struct Error {
     #[serde(rename = "data")]
     data: serde_json::Value,
 }
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, Hash, PartialEq, Default)]
-struct ID {
-    // At most one of Num or Str may be nonzero. If both are zero
-    // valued, then IsNum specifies which field's value is to be used
-    // as the ID.
-    num: u64,
-    str: String,
-    // IsString controls whether the Num or Str field's value should be
-    // used as the ID, when both are zero valued. It must always be
-    // set to true if the request ID is a string.
-    is_string: bool,
+//#[derive(Debug, Clone, Serialize, Deserialize, Eq, Hash, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Hash, Eq, Deserialize, Serialize, PartialOrd, Ord)]
+#[serde(untagged)]
+enum Id {
+    Number(u64),
+    Str(String),
+    Null,
 }
 
-impl ID {
-    fn string(&self) -> String {
-        if self.is_string {
-            return format!("\"{}\"", self.str);
-        }
-        self.num.to_string()
-    }
+impl Id {}
 
-    fn marshal_json(&self) -> Vec<u8> {
-        if self.is_string {
-            return self.str.as_bytes().to_vec();
-        }
-        self.num.to_be_bytes().to_vec()
-    }
-
-    fn unmarshal_json(&self, data: Vec<u8>) {
-
-        //u32::from_be_bytes(data)
-    }
-}
+pub type ResponseSender = mpsc::UnboundedSender<String>;
+pub type ResponseReceiver = mpsc::UnboundedReceiver<String>;
 
 struct Conn {
-    stream: Arc<Mutex<dyn ObjectStream<AnyMessage> + Send + Sync>>,
+    stream: Arc<Mutex<dyn ObjectStream<String> + Send + Sync>>,
     handler: Arc<dyn Handler + Send + Sync>,
     shut_down: bool,
     closing: bool,
-    seq: u64,
-    pending: HashMap<ID, Option<Arc<Call>>>,
+    seq: AtomicU64,
+    response_senders: Arc<Mutex<HashMap<Id, ResponseSender>>>,
 }
 
 impl Conn {
     fn new(
-        stream: Arc<Mutex<dyn ObjectStream<AnyMessage> + Send + Sync>>,
+        stream: Arc<Mutex<dyn ObjectStream<String> + Send + Sync>>,
         h: Arc<dyn Handler + Send + Sync>,
     ) -> Self {
         Self {
@@ -127,8 +213,8 @@ impl Conn {
             handler: h,
             shut_down: false,
             closing: false,
-            seq: 0,
-            pending: HashMap::new(),
+            seq: AtomicU64::new(0),
+            response_senders: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
@@ -141,7 +227,7 @@ impl Conn {
         self.stream.lock().await.close().await
     }
 
-    async fn send(&mut self, msg: AnyMessage, wait: bool) -> Result<Option<Arc<Call>>> {
+    async fn send(&self, msg: AnyMessage) -> Result<()> {
         if self.shut_down || self.closing {
             return Err(JsonError::ErrClosed);
         }
@@ -167,24 +253,72 @@ impl Conn {
         //     self.seq += 1;
         // }
 
-        // if let Err(_) = self.stream.lock().await.write_object(msg).await {
-        //     self.pending.remove(&id);
-        // }
+        let send_msg = serde_json::to_string(&msg)?;
+        self.stream.lock().await.write_object(send_msg).await?;
+
+        Ok(())
+    }
+
+    async fn call<T>(
+        &self,
+        method: String,
+        params: Option<String>,
+        is_notify: bool,
+    ) -> Result<Option<T>>
+    where
+        T: DeserializeOwned,
+    {
+        let mut id: Option<Id> = None;
+
+        let (sender, mut receiver) = mpsc::unbounded_channel();
+        if !is_notify {
+            let id_value = Id::Number(self.seq.load(Ordering::Relaxed));
+            id = Some(id_value.clone());
+            self.seq.fetch_add(1, Ordering::Relaxed);
+            self.response_senders.lock().await.insert(id_value, sender);
+        }
+
+        let msg = AnyMessage::Request(Request::new(method, params, id));
+        self.send(msg).await?;
+
+        if is_notify {
+            return Ok(None);
+        }
+
+        if let Some(message) = receiver.recv().await {
+            return Ok(Some(serde_json::from_str::<T>(&message)?));
+        }
 
         Ok(None)
     }
 
-    fn call(
-        &self,
-        method: String,
-        params: serde_json::Value,
-        result: &serde_json::Value,
-    ) -> Result<()> {
-        Ok(())
-    }
+    async fn read_messages(slf: Arc<Mutex<Self>>) {
+        let s = slf.lock().await;
+        loop {
+            if let Ok(msg) = s.stream.lock().await.read_object().await {
+                // let mut any_message = AnyMessage::default();
+                // if let Err(err) = any_message.unmarshal_json(msg) {}
 
-    fn dispatch_call(&self, method: String, params: serde_json::Value) -> Result<()> {
-        Ok(())
+                if let Ok(any_message) = serde_json::from_str::<AnyMessage>(&msg) {
+                    match any_message {
+                        AnyMessage::Request(req) => {
+                            s.handler.handle(slf.clone(), req);
+                        }
+                        AnyMessage::Response(res) => {
+                            match s.response_senders.lock().await.get_mut(&res.id) {
+                                Some(sender) => {}
+                                None => {
+                                    log::error!(
+                                        "the responsd sender with id: {} is none",
+                                        serde_json::to_string(&res.id).unwrap()
+                                    );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -203,7 +337,7 @@ impl Waiter {
 }
 
 trait Handler {
-    fn handle(&self, conn: Conn, request: Request);
+    fn handle(&self, conn: Arc<Mutex<Conn>>, request: Request);
 }
 
 struct Call {
@@ -226,113 +360,48 @@ impl Call {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct AnyValueWithExplicitNull {
-    null: bool,
-    value: Option<serde_json::Value>,
+#[serde(untagged)]
+enum AnyMessage {
+    Request(Request),
+    Response(Response),
 }
-
-impl AnyValueWithExplicitNull {
-    fn marshal_json(&self) -> Result<Vec<u8>> {
-        Ok(serde_json::to_vec(&self.value)?)
-    }
-
-    fn unmarshal_json(&self, data: Vec<u8>) -> Result<AnyValueWithExplicitNull> {
-        let d = trim_ascii_whitespace(&data[..]);
-
-        let vv = str::from_utf8(d).unwrap();
-        if vv == "null" {
-            return Ok(AnyValueWithExplicitNull {
-                null: true,
-                value: None,
-            });
-        }
-        let val: AnyValueWithExplicitNull = serde_json::from_slice(&data[..])?;
-        Ok(val)
-    }
-}
-
-pub fn trim_ascii_whitespace(x: &[u8]) -> &[u8] {
-    let from = match x.iter().position(|x| !x.is_ascii_whitespace()) {
-        Some(i) => i,
-        None => return &x[0..0],
-    };
-    let to = x.iter().rposition(|x| !x.is_ascii_whitespace()).unwrap();
-    &x[from..=to]
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct AnyMessage {
-    request: Option<Request>,
-    response: Option<Response>,
-}
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Msg {
-    #[serde(rename = "id")]
-    id: serde_json::Value,
-    #[serde(rename = "method")]
-    method: String,
-    #[serde(rename = "result")]
-    result: Option<serde_json::Value>,
-    #[serde(rename = "error")]
-    error: Option<serde_json::Value>,
-}
+// #[derive(Debug, Clone, Serialize, Deserialize)]
+// struct Msg {
+//     #[serde(rename = "id")]
+//     id: serde_json::Value,
+//     #[serde(rename = "method")]
+//     method: String,
+//     #[serde(rename = "result")]
+//     result: Option<serde_json::Value>,
+//     #[serde(rename = "error")]
+//     error: Option<serde_json::Value>,
+// }
 
 impl AnyMessage {
-    pub fn unmarshal_json(&mut self, data: String) -> Result<()> {
-        let any_msg: Msg = serde_json::from_str(&data)?;
+    // pub fn marshal_json(&self) -> Result<String> {
+    //     // if let Some(request) = &self.request {
+    //     //     return Ok(serde_json::to_string(request)?);
+    //     // } else if let Some(response) = &self.response {
+    //     //     return Ok(serde_json::to_string(response)?);
+    //     // }
 
-        if any_msg.method.is_empty() {
-            let msg: Request = serde_json::from_str(&data)?;
-            self.request = Some(msg);
-            return Ok(());
-        } else if any_msg.result.is_some() || any_msg.error.is_some() {
-            let msg: Response = serde_json::from_str(&data)?;
-            self.response = Some(msg);
-            return Ok(());
-        }
-        // let mut is_request: bool = false;
-        // let mut is_response: bool = false;
+    //     serde_json::to_string(self)?;
 
-        // let mut check_type = |m: Msg| -> Result<()> {
-        //     let m_is_request = !m.method.is_empty();
-        //     let m_is_response = m.result.null || m.result.value.is_some() || m.error.is_some();
+    //     Err(JsonError::ErrIllegalMessage)
+    // }
+    // pub fn unmarshal_json(&mut self, data: String) -> Result<()> {
+    //     // let any_msg: Msg = serde_json::from_str(&data)?;
 
-        //     if (!m_is_request && !m_is_response) || (m_is_request && m_is_response) {
-        //         return Err(JsonError::ErrUnableDetermineMsgType);
-        //     }
-        //     if (m_is_request && is_response) || (m_is_response && is_request) {
-        //         return Err(JsonError::ErrUnableDetermineMsgType);
-        //     }
+    //     // if any_msg.method.is_empty() {
+    //     //     let msg: Request = serde_json::from_str(&data)?;
+    //     //     self.request = Some(msg);
+    //     //     return Ok(());
+    //     // } else if any_msg.result.is_some() || any_msg.error.is_some() {
+    //     //     let msg: Response = serde_json::from_str(&data)?;
+    //     //     self.response = Some(msg);
+    //     //     return Ok(());
+    //     // }
 
-        //     is_request = m_is_request;
-        //     is_response = m_is_response;
-
-        //     Ok(())
-        // };
-
-        // let is_array = (data.len() > 0) && (data[0] as char == '[');
-
-        // if is_array {
-        //     let msgs: Vec<Msg> = serde_json::from_slice(&data[..])?;
-        //     if msgs.len() == 0 {
-        //         return Err(JsonError::ErrInvalidEmptyBatch);
-        //     }
-        //     for msg in msgs {
-        //         check_type(msg)?;
-        //     }
-        // } else {
-        //     let msg: Msg = serde_json::from_slice(&data[..])?;
-        //     check_type(msg)?;
-        // }
-
-        // if is_request && !is_response {
-        //     // let request: Request = serde_json::from_slice(&data[..])?;
-        //     // self.request = Some(request);
-        // } else if !is_request && is_response {
-        //     let response: Response = serde_json::from_slice(&data[..])?;
-        //     self.response = Some(response);
-        // }
-
-        Err(JsonError::ErrIllegalMessage)
-    }
+    //     Err(JsonError::ErrIllegalMessage)
+    // }
 }
